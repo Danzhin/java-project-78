bc95640ecc1050f74e862e136faf44ba77f52fee
dronz <dronzhin123@gmail,com>
2024-11-06 16:22:29 +0300

    update

--- app/src/test/java/hexlet/code/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -11,8 +11,6 @@ public class StringSchemaTest {
     private final Validator VALIDATOR = new Validator();
     private StringSchema schema;
 
-    private final String testString = "abc";
-
     @BeforeEach
     public void beforeEach() {
         schema = VALIDATOR.string();
@@ -20,26 +18,26 @@ public class StringSchemaTest {
 
     @Test
     public void testRequired() {
-        assertTrue(schema.isValid(""));
         assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(""));
         schema.required();
-        assertFalse(schema.isValid(""));
         assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
     }
 
     @Test
     public void testMinLength() {
         schema.minLength(3);
-        assertTrue(schema.isValid(testString));
+        assertTrue(schema.isValid("abc"));
         schema.minLength(4);
-        assertFalse(schema.isValid(testString));
+        assertFalse(schema.isValid("abc"));
     }
 
     @Test
     public void testContains() {
         schema.contains("bc");
-        assertTrue(schema.isValid(testString));
+        assertTrue(schema.isValid("abc"));
         schema.contains("cb");
-        assertFalse(schema.isValid(testString));
+        assertFalse(schema.isValid("abc"));
     }
 }
