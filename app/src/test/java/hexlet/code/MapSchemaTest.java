d38ab663332d1cfee294eca6c6198a4c0690d262
dronz <dronzhin123@gmail,com>
2024-11-07 14:08:12 +0300

    update

--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -22,19 +21,15 @@ public class MapSchemaTest {
     @Test
     public void testRequired() {
         schema.required();
-        Map<String, String> map = new HashMap<>();
-        assertTrue(schema.isValid(map));
+        assertTrue(schema.isValid(Map.of()));
         assertFalse(schema.isValid(null));
     }
 
     @Test
     public void testSizeof() {
         schema.sizeof(1);
-        Map<String, String> map = new HashMap<>();
-        map.put("key1", "value1");
-        assertTrue(schema.isValid(map));
-        map.remove("key1");
-        assertFalse(schema.isValid(map));
+        assertTrue(schema.isValid(Map.of("key1", "value1")));
+        assertFalse(schema.isValid(Map.of()));
     }
 
 }
