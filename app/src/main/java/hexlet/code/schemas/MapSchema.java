efefe22bddd3c0f69574c4ebc54614a5a6e05aba
dronz <dronzhin123@gmail,com>
2024-11-18 13:35:51 +0300

    update

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,7 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public final class MapSchema<T> extends BaseSchema<Map<T, T>> {
+public final class MapSchema<T> extends BaseSchema<Map<?, ?>> {
 
     private boolean requiredFilling = false;
     private Integer minSize = null;
@@ -34,10 +34,10 @@ public final class MapSchema<T> extends BaseSchema<Map<T, T>> {
     }
 
     @Override
-    public boolean isValid(Map<T, T> map) {
+    public boolean isValid(Map<?, ?> map) {
         return (!requiredFilling || map != null)
                 && (minSize == null || map.size() >= minSize)
-                && (shapeSchemes == null || checkShape(map));
+                && (shapeSchemes == null || checkShape((Map<T, T>) map));
     }
 
 }
