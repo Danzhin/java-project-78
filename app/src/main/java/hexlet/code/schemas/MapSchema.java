14fb44c6901129786ca87e8fd92e4b82d8523cbe
dronz <dronzhin123@gmail,com>
2024-11-19 16:28:43 +0300

    update

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,21 +3,34 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.Objects;
 
-public final class MapSchema<T> extends BaseSchema<Map<?, ?>> {
+public final class MapSchema<T1, T2> extends BaseSchema<Map<T1, T2>> {
 
-    public MapSchema<T> required() {
+    public MapSchema<T1, T2> required() {
         addChecks(Objects::nonNull);
         return this;
     }
 
-    public MapSchema<T> sizeof(int number) {
+    public MapSchema<T1, T2> sizeof(int number) {
         addChecks(map -> map.size() == number);
         return this;
     }
 
-    //public MapSchema<T> shape(Map<T, BaseSchema<T>> schemas) {
-        //addChecks(value -> schemas.entrySet().stream().allMatch(e -> e.getValue().isValid(value.get(e.getKey()))));
-        //return this;
-    //}
+    public MapSchema<T1, T2> shape(Map<T1, BaseSchema<T2>> schemes) {
+        addChecks(map -> {
+            if (map == null || schemes == null) {
+                return true;
+            }
+            for (Map.Entry<T1, BaseSchema<T2>> entry : schemes.entrySet()) {
+                T1 key = entry.getKey();
+                BaseSchema<T2> schema = entry.getValue();
+
+                if (!map.containsKey(key) || !schema.isValid(map.get(key))) {
+                    return false;
+                }
+            }
+            return true;
+        });
+        return this;
+    }
 
 }
