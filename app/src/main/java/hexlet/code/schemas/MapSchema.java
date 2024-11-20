1cf741ec37ef5c02a57972a5bd17b9933795befc
dronz <dronzhin123@gmail,com>
2024-11-20 09:31:57 +0300

    update

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.Objects;
 
-public final class MapSchema extends BaseSchema<Map<?, ?>> {
+public final class MapSchema extends BaseSchema<Map<String, Object>> {
 
     public MapSchema required() {
         addCheck(Objects::nonNull);
