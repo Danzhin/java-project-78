40b2075a0a8b8c276dbf69d23259a244ff34709a
dronz <dronzhin123@gmail,com>
2024-11-20 09:47:01 +0300

    update

--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MapSchemaTest {
 
     private final Validator validator = new Validator();
-    private MapSchema<String> schema;
+    private MapSchema schema;
 
     @BeforeEach
     public void beforeEach() {
