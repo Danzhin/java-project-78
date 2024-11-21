4a25774181de77ce1356d7323b2f3cd6b74f2b16
dronz <dronzhin123@gmail,com>
2024-11-21 08:46:39 +0300

    update

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,19 +3,19 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.Objects;
 
-public final class MapSchema extends BaseSchema<Map<?, ?>> {
+public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
 
-    public MapSchema required() {
+    public MapSchema<T> required() {
         addCheck(Objects::nonNull);
         return this;
     }
 
-    public MapSchema sizeof(int size) {
+    public MapSchema<T> sizeof(int size) {
         addCheck(map -> map != null && map.size() == size);
         return this;
     }
 
-    public MapSchema shape(Map<String, BaseSchema<Object>> schemes) {
+    public MapSchema<T> shape(Map<String, BaseSchema<T>> schemes) {
         addCheck(map -> schemes.entrySet().stream()
                 .allMatch(entry -> map.containsKey(entry.getKey())
                         && entry.getValue().isValid(map.get(entry.getKey()))));
